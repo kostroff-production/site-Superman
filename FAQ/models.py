3223bb3218c84e35fa11699cdbf414d4aca862c0
Kostrov-Producsion <44669971+Kostrov-Producsion@users.noreply.github.com>
2020-08-23 01:09:57 +0300

    Update models.py

--- FAQ/models.py
+++ FAQ/models.py
@@ -2,12 +2,14 @@ from django.db import models
 from django.shortcuts import reverse
 from django.utils.text import slugify
 from time import time
+from utils.main import disable_for_loaddata
 
 
 def gen_slug(s):
 	new_slug = slugify(s, allow_unicode=True)
 	return new_slug + '-' + str(int(time()))
 
+@disable_for_loaddata
 class Post(models.Model):
 	title = models.CharField(max_length=150, db_index=True)
 	slug = models.SlugField(max_length=150, blank=True, unique=True)
